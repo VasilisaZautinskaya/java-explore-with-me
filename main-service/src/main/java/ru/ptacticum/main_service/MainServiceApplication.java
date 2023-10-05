949383314a97dd3e6ef732a29b90d7ff5c5901ee
Василиса <vasilisaraist@yandex.ru>
2023-10-05 14:09:33 +0300

    - созданы модель user и ДТО; - создана БД;

--- main-service/src/main/java/ru/ptacticum/main_service/MainServiceApplication.java
+++ main-service/src/main/java/ru/ptacticum/main_service/MainServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.mainservice;
+package ru.ptacticum.main_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ main-service/src/main/java/ru/ptacticum/main_service/user/mapper/UserMapper.java
@@ -0,0 +1,27 @@
+package ru.ptacticum.main_service.user.mapper;
+
+import lombok.experimental.UtilityClass;
+import ru.ptacticum.main_service.user.dto.UserDto;
+import ru.ptacticum.main_service.user.dto.UserShortDto;
+import ru.ptacticum.main_service.user.model.User;
+
+@UtilityClass
+public class UserMapper {
+
+    public UserDto returnUserDto(User user) {
+        UserDto userDto = UserDto.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .name(user.getName())
+                .build();
+        return userDto;
+    }
+
+    public UserShortDto returnUserShortDto(User user) {
+        UserShortDto userShortDto = UserShortDto.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .build();
+        return userShortDto;
+    }
+}
