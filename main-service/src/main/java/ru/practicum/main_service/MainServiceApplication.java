9481ba5a204619b231c80639699c96bbf96b9cb4
Василиса <vasilisaraist@yandex.ru>
2023-10-07 19:58:58 +0300

    - контейнеры запускаются;

--- main-service/src/main/java/ru/practicum/main_service/MainServiceApplication.java
+++ main-service/src/main/java/ru/practicum/main_service/MainServiceApplication.java
@@ -2,7 +2,9 @@ package ru.practicum.main_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = "ru.practicum")
 @SpringBootApplication
 public class MainServiceApplication {
 
