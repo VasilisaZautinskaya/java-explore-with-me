34df3c86fb7f18eb4ba37a3361390d80a1f3958b
Василиса <vasilisaraist@yandex.ru>
2023-10-10 03:21:00 +0300

    - последние изменения;

--- stats-service/stats-server/src/main/java/ru/practicum/statsserver/service/StatsService.java
+++ stats-service/stats-server/src/main/java/ru/practicum/statsserver/service/StatsService.java
@@ -4,11 +4,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.statsserver.exception.ValidationException;
 import ru.practicum.statsserver.model.Stats;
 import ru.practicum.statsserver.model.ViewStats;
 import ru.practicum.statsserver.repository.StatsRepositoryImpl;
 
-import javax.validation.ValidationException;
 import java.time.LocalDateTime;
 import java.util.List;
 
